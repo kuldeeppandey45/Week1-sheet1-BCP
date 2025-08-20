f203ac22f1031d07b3e97723ecbce322e047c7e0
kuldeeppandey45 <kuldeeppandey8938055619@gmail.com>
2025-08-20 23:52:55 +0530

    Create Q1divide_by_7.py

---
+++ Q1divide_by_7.py
@@ -0,0 +1,7 @@
+b= int (input("enter a number: "))
+c= int (input("enter a number: "))
+if a > b and b < c and c > a:
+    print("Triangle is rectangle")
+else:
+    print("Triangle is not outside")
+   
